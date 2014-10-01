964edd95de7ab8556f8808e0d404763ea2be2ebe
leo <dleottau@ing.uchile.cl>
2014-09-30 23:49:54 -0300

    convergiendo en 1000 episodios sin transfer

--- RUN_SCRIPT.m
+++ RUN_SCRIPT.m
@@ -2,7 +2,7 @@ clear all
 clc
 close all
 
-episodes = 1000;   % maximum number of  episode
+episodes = 2000;   % maximum number of  episode
 maxDistance = 6000;    % maximum ball distance permited before to end the episode X FIELD DIMENSION
 th_max = [250 15 15];      % maximum pho desired
 Runs = 2;
@@ -11,7 +11,7 @@ tic
 DRAWS = 1;
 
 Q_INIT = 0;
-TRANSFER = 1;  %=1 transfer, >1 acts gready from source policy, =0 learns from scratch,
+TRANSFER = 0;  %=1 transfer, >1 acts gready from source policy, =0 learns from scratch,
 EXPL_EPISODES_FACTOR = 3;
 
 a_spot={'r' 'g' 'b' 'c' 'm' 'y' 'k' '--r' '--g' '--b' '--c' };
