4f4c6364fc2abf0f3941d8f9504d42f2a420924e
leo <dleottau@ing.uchile.cl>
2015-12-02 18:42:07 -0300

    Temporal branch to solve old conflicts

--- RL-TestBedProblems/passing/dist.m
+++ RL-TestBedProblems/passing/dist.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [ d ] = dist( p1,p2 )
 
 d = sqrt((p1(1)-p2(1))^2+(p2(2)-p1(2))^2);
@@ -6,12 +5,3 @@ d = sqrt((p1(1)-p2(1))^2+(p2(2)-p1(2))^2);
 
 end
 
-=======
-function [ d ] = dist( p1,p2 )
-
-d = sqrt((p1(1)-p2(1))^2+(p2(2)-p1(2))^2);
-
-
-end
-
->>>>>>> origin/thesis
